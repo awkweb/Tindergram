115fff3a6bd50d6c0977097563791377d7b5dc8e
Thomas Meagher <thom.meagher@gmail.com>
2015-04-09 09:10:29 -0400

    Decoupled Swipe and Card Views, Refactor

--- Tindergram/CardsViewController.swift
+++ Tindergram/CardsViewController.swift
@@ -10,30 +10,40 @@ import UIKit
 
 class CardsViewController: UIViewController {
   
+  struct Card {
+    let cardView: CardView
+    let swipeView: SwipeView
+  }
+  
   let frontCardTopMargin: CGFloat = 0
   let backCardTopMargin: CGFloat = 10
   
   @IBOutlet weak var cardStackView: UIView!
   
-  var backCard: SwipeView?
-  var frontCard: SwipeView?
+  var backCard: Card?
+  var frontCard: Card?
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
     cardStackView.backgroundColor = UIColor.clearColor()
     
-    backCard = SwipeView(frame: createCardFrame(backCardTopMargin))
-    backCard!.delegate = self
-    cardStackView.addSubview(backCard!)
-    
-    frontCard = SwipeView(frame: createCardFrame(frontCardTopMargin))
-    frontCard!.delegate = self
-    cardStackView.addSubview(frontCard!)
+    backCard = createCard(backCardTopMargin)
+    cardStackView.addSubview(backCard!.swipeView)
+    frontCard = createCard(frontCardTopMargin)
+    cardStackView.addSubview(frontCard!.swipeView)
   }
 
   private func createCardFrame(topMargin: CGFloat) -> CGRect {
     return CGRect(x: 0, y: topMargin, width: cardStackView.frame.width, height: cardStackView.frame.height)
+  }
+  
+  private func createCard(topMargin: CGFloat) -> Card {
+    let cardView = CardView()
+    let swipeView = SwipeView(frame: createCardFrame(topMargin))
+    swipeView.delegate = self
+    swipeView.innerView = cardView
+    return Card(cardView: cardView, swipeView: swipeView)
   }
   
 }
@@ -44,14 +54,14 @@ extension CardsViewController: SwipeViewDelegate {
   func swipedLeft() {
     println("left")
     if let frontCard = frontCard {
-      frontCard.removeFromSuperview()
+      frontCard.swipeView.removeFromSuperview()
     }
   }
   
   func swipedRight() {
     println("right")
     if let frontCard = frontCard {
-      frontCard.removeFromSuperview()
+      frontCard.swipeView.removeFromSuperview()
     }
   }
   
--- Tindergram/SwipeView.swift
+++ Tindergram/SwipeView.swift
@@ -25,7 +25,15 @@ class SwipeView: UIView {
   
   weak var delegate: SwipeViewDelegate?
   
-  private let card: CardView = CardView()
+  var innerView: UIView? {
+    didSet {
+      if let v = innerView {
+        addSubview(v)
+        v.frame = CGRect(x: 0, y: 0, width: frame.width, height: frame.height)
+      }
+    }
+  }
+  
   private var originalPoint: CGPoint?
   
   required init(coder aDecoder: NSCoder) {
@@ -45,10 +53,8 @@ class SwipeView: UIView {
   
   private func initialize() {
     backgroundColor = UIColor.redColor() // Change back to clearColor
-    addSubview(card)
     
     addGestureRecognizer(UIPanGestureRecognizer(target: self, action: "dragged:"))
-    card.frame = CGRect(x: 0, y: 0, width: frame.width, height: frame.height)
   }
   
   func dragged(gestureRecognizer: UIPanGestureRecognizer) {
